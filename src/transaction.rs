4d03e98271de8881d33f8a85146392f73b24d50b
jim-nnamdi <jimsamuel50@gmail.com>
2023-12-04 23:27:08 +0100

    transaction pool base impl

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,7 +1,7 @@
-use std::time::SystemTime;
-use anyhow::{Result, Ok};
+use anyhow::{Ok, Result};
 use rsa::{RsaPrivateKey, RsaPublicKey};
 use secp256k1::{PublicKey, SecretKey};
+use std::time::SystemTime;
 
 use crate::{account::Account, block::Block};
 
@@ -27,7 +27,12 @@ pub struct TransactionPool {
 }
 
 impl Transaction {
-    pub fn new(user:Account, user_secret: SecretKey, blockdata:Block, amount: u32) -> Result<Transaction> {
+    pub fn new(
+        user: Account,
+        user_secret: SecretKey,
+        blockdata: Block,
+        amount: u32,
+    ) -> Result<Transaction> {
         let secp = secp256k1::Secp256k1::new();
         let user_ref = PublicKey::from_secret_key(&secp, &user_secret);
         if !user.acc_public.eq(&user_ref) || user.acc_balance.ge(&amount) {
@@ -36,11 +41,39 @@ impl Transaction {
             println!("invalid secret key or insufficient balance");
         }
         let mut rng = rand::thread_rng();
-        let bit_size  = 512;
+        let bit_size = 512;
         let tx_id = RsaPrivateKey::new(&mut rng, bit_size).unwrap();
         let tx_hash = RsaPublicKey::from(&tx_id);
-        let txheader = TxHeader{ transaction_id: tx_id, transaction_hash:tx_hash, transaction_valid:true};
-        let newtx = Transaction{tx_user:user, timestamp:SystemTime::now(),tx_header:txheader,tx_block:blockdata, tx_amount:amount};
+        let txheader = TxHeader {
+            transaction_id: tx_id,
+            transaction_hash: tx_hash,
+            transaction_valid: true,
+        };
+        let newtx = Transaction {
+            tx_user: user,
+            timestamp: SystemTime::now(),
+            tx_header: txheader,
+            tx_block: blockdata,
+            tx_amount: amount,
+        };
         Ok(newtx)
+    }
+}
+
+impl TransactionPool {
+    pub fn fetch_txs(&self) {
+        let alltx = self
+            .transactions
+            .iter()
+            .map(|f| f.tx_block.clone())
+            .collect::<Vec<_>>();
+          log::info!("[txs]:{:?}",alltx);
+          for tx in &self.transactions {
+            println!("tx = {:?}", tx);
+        }
+    }
+    pub fn add_to_pool(&mut self, tx:Transaction) ->Result<bool> {
+       self.transactions.push(tx);
+       Ok(true)
     }
 }

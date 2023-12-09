d97c9b24b01ca0acb92f8e704164e3e89f644ee1
jim-nnamdi <jimsamuel50@gmail.com>
2023-12-09 16:23:41 +0100

    Random BlockData Saved & Appended Inside BlockTxt File DB for Re-Use

--- src/block.rs
+++ src/block.rs
@@ -1,7 +1,7 @@
-use core::panic;
 use std::{
-    rc::Rc,
-    time::{Duration, SystemTime}, mem, io::Write
+    io::Write,
+    mem,
+    time::{Duration, SystemTime},
 };
 
 use anyhow::{Ok, Result};
@@ -10,11 +10,14 @@ use secp256k1::hashes::sha256;
 use secp256k1::{Message, Secp256k1};
 use serde::{Deserialize, Serialize};
 
-use crate::{account::Account, sec8::{self, sec8_block_id_hash}};
+use crate::{
+    account::Account,
+    sec8::{self, sec8_block_id_hash},
+};
 const VMAX: u32 = 30;
 pub trait _BlockT {
     fn new() -> Self;
-    fn create_essex_block(block:Block, acc:Account, msg:&str)->Result<Block>;
+    fn create_essex_block(block: Block, acc: Account, msg: &str) -> Result<Block>;
     fn validate_block(prevblock: Block) -> Result<bool>;
 }
 
@@ -33,7 +36,7 @@ impl Default for Block {
     fn default() -> Self {
         let sec8_ks = sec8_block_id_hash().unwrap();
         let sec8_mess = Message::from_hashed_data::<sha256::Hash>("".as_bytes());
-        let sec8_sg = secp256k1::Secp256k1::sign_ecdsa(&Secp256k1::new(),&sec8_mess, &sec8_ks.0);
+        let sec8_sg = secp256k1::Secp256k1::sign_ecdsa(&Secp256k1::new(), &sec8_mess, &sec8_ks.0);
         Block {
             block_hash: sec8_ks.0.display_secret().to_string(),
             prev_hash: sec8_ks.0.display_secret().to_string(),
@@ -42,7 +45,8 @@ impl Default for Block {
             signature: sec8_sg.to_string(),
             valid: false,
             timestamp: SystemTime::now(),
-        }.to_owned()
+        }
+        .to_owned()
     }
 }
 
@@ -52,11 +56,13 @@ impl _BlockT for Block {
     }
 
     // pass same message used in acc creation ...
-    fn create_essex_block(mut block:Block, acc:Account, msg:&str) -> Result<Block> {
+    fn create_essex_block(mut block: Block, acc: Account, msg: &str) -> Result<Block> {
         // let's assume the genesis block was passed in the fn
         let check_validated = Self::validate_block(mem::take(&mut block)).is_ok();
         // by default the genesis block should pass this logic
-        if !check_validated {log::error!("invalid previous block");}
+        if !check_validated {
+            log::error!("invalid previous block");
+        }
         // secp256k1 algorithm is to be used for signatures
         let scp = secp256k1::Secp256k1::new();
         // msg is same as the one user had during acc creation
@@ -65,29 +71,64 @@ impl _BlockT for Block {
         // 0x1E min val a validator should have to create block
         let vok = ubal > VMAX;
         // validator loses coins because of illegal tx
-        if !vok{log::error!("insufficient balance");}
-        let essex_secret = sec8::sec8_block_id_hash().unwrap().0.display_secret().to_string();
-        let block = Block{block_hash:essex_secret,prev_hash:block.block_hash,validator:acc.acc_public.to_string(),signature:acc.acc_signed.to_string(),block_data:vec![],valid:true,timestamp:SystemTime::now()};
-        let mut data_store = std::fs::File::open("./block.txt")?;
-        let serialise_block = serde_json::to_string(&block)?;
-        let _ = data_store.write_all(serialise_block.as_bytes())?;
+        if !vok {
+            log::error!("insufficient balance");
+        }
+        let essex_secret = sec8::sec8_block_id_hash()
+            .unwrap()
+            .0
+            .display_secret()
+            .to_string();
+        let block = Block {
+            block_hash: essex_secret,
+            prev_hash: block.block_hash,
+            validator: acc.acc_public.to_string(),
+            signature: acc.acc_signed.to_string(),
+            block_data: vec![],
+            valid: true,
+            timestamp: SystemTime::now(),
+        };
+        let data_store = std::fs::File::options().append(true).open("block.txt");
+        match data_store {
+            core::result::Result::Ok(mut data) => {
+                let serialise_block = serde_json::to_string(&block);
+                match serialise_block {
+                    core::result::Result::Ok(resb) => {
+                        data.write_all(resb.as_bytes())?;
+                        return Ok(block);
+                    }
+                    Err(e) => {
+                        eprintln!("{}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                println!("could not save data = {}", e);
+                return Ok(block.clone());
+            }
+        }
         // verification on the validator data
-        scp.verify_ecdsa(&mex, &acc.acc_signed, &acc.acc_public).unwrap();
+        scp.verify_ecdsa(&mex, &acc.acc_signed, &acc.acc_public)
+            .unwrap();
         Ok(block)
     }
 
     fn validate_block(prevblock: Block) -> Result<bool> {
         let prev_valid = prevblock.valid;
         if prev_valid {
-                // if previous block is valid get creation time
-                let block_creation_time = prevblock.timestamp;
-                // check_add to see that block is > 0x02 hours
-                let ox02 = SystemTime::now()
-                    .checked_add(Duration::from_secs(7200)).ok_or("invalid time").unwrap();
-                // fetch the duration since the block creation time
-                let block_time_diff = ox02.duration_since(block_creation_time)?;
-                // if the block difference is < 0x02 hours validate!
-                if block_time_diff < Duration::from_secs(7200) {return Ok(true);}
+            // if previous block is valid get creation time
+            let block_creation_time = prevblock.timestamp;
+            // check_add to see that block is > 0x02 hours
+            let ox02 = SystemTime::now()
+                .checked_add(Duration::from_secs(7200))
+                .ok_or("invalid time")
+                .unwrap();
+            // fetch the duration since the block creation time
+            let block_time_diff = ox02.duration_since(block_creation_time)?;
+            // if the block difference is < 0x02 hours validate!
+            if block_time_diff < Duration::from_secs(7200) {
+                return Ok(true);
+            }
         }
         Ok(false)
     }
